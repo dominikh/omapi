89c4cfdbda75464ae43e0a3b828926f0181746aa
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-26 23:39:49 +0100

    initial commit

---
+++ omapi.go
@@ -0,0 +1,470 @@
+package main // FIXME omapi
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
+	"sort"
+	"os"
+)
+
+type Opcode int32
+
+const (
+	OpOpen Opcode = 1 + iota
+	OpRefresh
+	OpUpdate
+	OpNotify
+	OpStatus
+	OpDelete
+)
+
+var Ethernet = []byte{0, 0, 0, 1}
+
+func (opcode Opcode) String() (ret string) {
+	switch opcode {
+	case 1:
+		ret = "open"
+	case 2:
+		ret = "refresh"
+	case 3:
+		ret = "update"
+	case 4:
+		ret = "notify"
+	case 5:
+		ret = "status"
+	case 6:
+		ret = "delete"
+	}
+
+	return
+}
+
+// TODO add size checks for all operations
+type Buffer struct {
+	buffer *bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{new(bytes.Buffer)}
+}
+
+func (b *Buffer) add_bytes(data []byte) {
+	b.buffer.Write(data)
+}
+
+func (b *Buffer) add(data interface{}) {
+	err := binary.Write(b.buffer, binary.BigEndian, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (b *Buffer) add_map(data map[string][]byte) {
+	// We need to add the map in a deterministic order for signing to
+	// work, so we first sort the keys in alphabetical order, then use
+	// that order to access the map entries.
+
+	keys := make(sort.StringSlice, 0, len(data))
+
+	for key := range data {
+		keys = append(keys, key)
+	}
+
+	sort.Sort(keys)
+
+	for _, key := range keys {
+		value := data[key]
+
+		b.add(int16(len(key)))
+		b.add([]byte(key))
+
+		b.add(int32(len(value)))
+		b.add(value)
+	}
+
+	b.add([]byte("\x00\x00"))
+}
+
+func (b *Buffer) Bytes() []byte {
+	return b.buffer.Bytes()
+}
+
+type Message struct {
+	AuthID    int32
+	Opcode    Opcode
+	Handle    int32
+	Tid       int32
+	Rid       int32
+	Message   map[string][]byte
+	Object    map[string][]byte
+	Signature []byte
+}
+
+func NewMessage() *Message {
+	msg := &Message{
+		Tid:     rand.Int31(),
+		Message: make(map[string][]byte),
+		Object:  make(map[string][]byte),
+	}
+
+	return msg
+}
+
+func NewOpenMessage(typeName string) *Message {
+	message := NewMessage()
+	message.Opcode = OpOpen
+	message.Message["type"] = []byte(typeName)
+
+	return message
+}
+
+func NewDeleteMessage(handle int32) *Message {
+	message := NewMessage()
+	message.Opcode = OpDelete
+	message.Handle = handle
+
+	return message
+}
+
+func (m *Message) Bytes(forSigning bool) []byte {
+	ret := NewBuffer()
+	if !forSigning {
+		ret.add(m.AuthID)
+	}
+
+	ret.add(int32(len(m.Signature)))
+	ret.add(m.Opcode)
+	ret.add(m.Handle)
+	ret.add(m.Tid)
+	ret.add(m.Rid)
+	ret.add_map(m.Message)
+	ret.add_map(m.Object)
+	if !forSigning {
+		ret.add(m.Signature)
+	}
+
+	return ret.buffer.Bytes()
+}
+
+func (m *Message) Sign(auth Authenticator) {
+	m.AuthID = auth.AuthID()
+	m.Signature = auth.Sign(m)
+}
+
+func (m *Message) Verify(auth Authenticator) bool {
+	return bytes.Equal(auth.Sign(m), m.Signature)
+}
+
+func (m *Message) IsResponseTo(other *Message) bool {
+	return m.Rid == other.Tid
+}
+
+type Connection struct {
+	Hostname      string
+	Port          int
+	Username      string
+	Key           string
+	Authenticator Authenticator
+	connection    *net.TCPConn
+	inBuffer      *bytes.Buffer
+}
+
+func NewConnection(hostname string, port int, username string, key string) *Connection {
+	con := &Connection{
+		Hostname:      hostname,
+		Port:          port,
+		Username:      username,
+		Key:           key,
+		Authenticator: new(NullAuthenticator),
+		inBuffer:      new(bytes.Buffer),
+	}
+
+	var newAuth Authenticator = new(NullAuthenticator)
+
+	if len(username) > 0 && len(key) > 0 {
+		decodedKey, err := base64.StdEncoding.DecodeString(key)
+		if err != nil {
+			panic(err)
+		}
+		newAuth = &HMACMD5Authenticator{username, decodedKey, -1}
+	}
+
+	raddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", hostname, port))
+	if err != nil {
+		// TODO return the error instead
+		panic(err)
+	}
+	tcpConn, err := net.DialTCP("tcp", nil, raddr)
+	if err != nil {
+		// TODO return the error instead
+		panic(err)
+	}
+
+	con.connection = tcpConn
+
+	con.SendProtocolInitialization()
+	con.ReceiveProtocolInitialization()
+	con.initializeAuthenticator(newAuth)
+
+	return con
+}
+
+func (con *Connection) initializeAuthenticator(auth Authenticator) {
+	if _, ok := auth.(*NullAuthenticator); ok {
+		return
+	}
+
+	message := NewOpenMessage("authenticator")
+	for key, value := range auth.AuthObject() {
+		message.Object[key] = value
+	}
+
+	response := con.queryServer(message)
+
+	if response.Opcode != OpUpdate {
+		panic("received non-update response for open")
+	}
+
+	if response.Handle == 0 {
+		panic("received invalid authid from server")
+	}
+
+	auth.SetAuthID(response.Handle)
+	con.Authenticator = auth
+}
+
+func (con *Connection) queryServer(msg *Message) *Message {
+	msg.Sign(con.Authenticator)
+	con.Send(msg.Bytes(false))
+	response := con.parseMessage()
+	if !response.IsResponseTo(msg) {
+		panic("received message is not the desired response")
+	}
+
+	// TODO check authid
+
+	return response
+}
+
+func (con *Connection) Send(data []byte) (n int, err error) {
+	return con.connection.Write(data)
+}
+
+func (con *Connection) SendProtocolInitialization() {
+	buf := NewBuffer()
+	buf.add(int32(100)) // Protocol version
+	buf.add(int32(24))  // Header size
+	con.Send(buf.Bytes())
+}
+
+func (con *Connection) Read() {
+	buf := make([]byte, 2048)
+	n, err := con.connection.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	con.inBuffer.Write(buf[0:n])
+}
+
+func (con *Connection) WaitForN(n int) {
+	for con.inBuffer.Len() < n {
+		con.Read()
+	}
+}
+
+func (con *Connection) parseStartupMessage() (version, headerSize int32) {
+	con.WaitForN(8)
+
+	binary.Read(con.inBuffer, binary.BigEndian, &version)
+	binary.Read(con.inBuffer, binary.BigEndian, &headerSize)
+
+	return
+}
+
+func (con *Connection) parseMap() map[string][]byte {
+	dict := make(map[string][]byte)
+
+	var (
+		keyLength   int16
+		valueLength int32
+		key         []byte
+		value       []byte
+	)
+
+	for {
+		con.WaitForN(2)
+		binary.Read(con.inBuffer, binary.BigEndian, &keyLength)
+		if keyLength == 0 {
+			// end of map
+			break
+		}
+
+		con.WaitForN(int(keyLength))
+		key = make([]byte, keyLength)
+		con.inBuffer.Read(key)
+
+		con.WaitForN(4)
+		binary.Read(con.inBuffer, binary.BigEndian, &valueLength)
+		con.WaitForN(int(valueLength))
+		value = make([]byte, valueLength)
+		con.inBuffer.Read(value)
+
+		dict[string(key)] = value
+	}
+
+	return dict
+}
+
+func (con *Connection) parseMessage() *Message {
+	message := new(Message)
+	con.WaitForN(24) // authid + authlen + opcode + handle + tid + rid
+
+	var authlen int32
+
+	binary.Read(con.inBuffer, binary.BigEndian, &message.AuthID)
+	binary.Read(con.inBuffer, binary.BigEndian, &authlen)
+	binary.Read(con.inBuffer, binary.BigEndian, &message.Opcode)
+	binary.Read(con.inBuffer, binary.BigEndian, &message.Handle)
+	binary.Read(con.inBuffer, binary.BigEndian, &message.Tid)
+	binary.Read(con.inBuffer, binary.BigEndian, &message.Rid)
+
+	message.Message = con.parseMap()
+	message.Object = con.parseMap()
+
+	con.WaitForN(int(authlen))
+	message.Signature = make([]byte, authlen)
+	con.inBuffer.Read(message.Signature)
+
+	return message
+}
+
+func (con *Connection) ReceiveProtocolInitialization() {
+	version, headerSize := con.parseStartupMessage()
+	if version != 100 {
+		panic("version mismatch")
+	}
+
+	if headerSize != 24 {
+		panic("header size mismatch")
+	}
+}
+
+type Authenticator interface {
+	Sign(*Message) []byte
+	AuthObject() map[string][]byte
+	AuthLen() int32
+	AuthID() int32
+	SetAuthID(int32)
+}
+
+type NullAuthenticator struct{}
+
+func (_ *NullAuthenticator) AuthObject() map[string][]byte {
+	return make(map[string][]byte)
+}
+
+func (_ *NullAuthenticator) Sign(_ *Message) []byte {
+	return []byte("")
+}
+
+func (_ *NullAuthenticator) AuthLen() int32 {
+	return 0
+}
+
+func (_ *NullAuthenticator) AuthID() int32 {
+	return 0
+}
+
+func (_ *NullAuthenticator) SetAuthID(_ int32) {
+}
+
+type HMACMD5Authenticator struct {
+	Username string
+	Key      []byte
+	_AuthID  int32
+}
+
+func (auth *HMACMD5Authenticator) AuthObject() map[string][]byte {
+	ret := make(map[string][]byte)
+	ret["name"] = []byte(auth.Username)
+	ret["algorithm"] = []byte("hmac-md5.SIG-ALG.REG.INT.")
+
+	return ret
+}
+
+func (auth *HMACMD5Authenticator) Sign(m *Message) []byte {
+	hmac := hmac.New(md5.New, auth.Key)
+
+	// The signature's length is part of the message that we are
+	// signing, so initialize the signature with the correct length.
+	m.Signature = bytes.Repeat([]byte("\x00"), int(auth.AuthLen()))
+	hmac.Write(m.Bytes(true))
+
+	return hmac.Sum(nil)
+}
+
+func (_ *HMACMD5Authenticator) AuthLen() int32 {
+	return 16
+}
+
+func (auth *HMACMD5Authenticator) AuthID() int32 {
+	return auth._AuthID
+}
+
+func (auth *HMACMD5Authenticator) SetAuthID(val int32) {
+	auth._AuthID = val
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	key := os.Getenv("OMAPI_KEY")
+	connection := NewConnection("192.168.1.1", 7911, "omapi_key", key)
+
+	// message := NewOpenMessage("lease")
+	// mac, _ := net.ParseMAC("bc:ae:c5:76:1d:5a")
+	// message.Object["hardware-address"] = []byte(mac)
+	// response := connection.queryServer(message)
+	// fmt.Println(response)
+
+	// message := NewOpenMessage("host")
+	// // message.Message["create"] = Ethernet
+	// mac, _ := net.ParseMAC("08:00:27:4f:72:21")
+	// message.Object["hardware-address"] = []byte(mac)
+
+	// // buf := new(bytes.Buffer)
+	// // binary.Write(buf, binary.BigEndian, int32(1))
+
+	// message.Object["hardware-type"] = Ethernet
+
+	// response := connection.queryServer(message)
+	// fmt.Println(response)
+	// response = connection.queryServer(NewDeleteMessage(response.Handle))
+	// fmt.Println(response)
+
+	mac, _ := net.ParseMAC("08:00:27:4f:72:21")
+	ip := net.ParseIP("192.168.1.33")
+	message := NewOpenMessage("host")
+
+	message.Message["create"] = []byte{0, 0, 0, 1}
+	message.Message["exclusive"] = []byte{0, 0, 0, 1}
+	message.Object["hardware-address"] = []byte(mac)
+	message.Object["hardware-type"] = Ethernet
+	message.Object["ip-address"] = []byte(ip[12:])
+	message.Object["statements"] = []byte("ddns-hostname=\"win7.vm\";")
+	message.Object["name"] = []byte("win7.vm")
+
+	response := connection.queryServer(message)
+	if response.Opcode != OpUpdate {
+		fmt.Println("add failed:", string(response.Message["message"]))
+	}
+	// fmt.Println(response)
+}
